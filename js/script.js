d51ad651acf7385f66c2c47cfee6255ac100d990
viktor1798 <vktrsmsk1798@gmail.com>
2022-09-29 20:13:57 +0300

    word script lesson 2.8(4) (source:learnjs.ru)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 //2.6
 
-let name = prompt("whats your name?");
-alert(`your name is ${name}`);
+/*let name = prompt("whats your name?");
+alert(`your name is ${name}`);*/
+
+//2.8 (4)
+
+/**/let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+
+//Правильная запись
+alert(Number(a)+Number(b));
+//Запись приобразования через унарный +
+alert(+a + +b);
