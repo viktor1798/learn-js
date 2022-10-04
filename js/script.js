559a8b7fbbca573d3cb8aa44215c9a15f63d0530
viktor1798 <vktrsmsk1798@gmail.com>
2022-10-04 11:40:41 +0300

    word script lesson 2.14(1,2) (source:learnjs.ru)

--- js/script.js
+++ js/script.js
@@ -184,3 +184,34 @@ if(user=="Админ"){
 
 //   alert( i ); 
 // }
+
+
+//2.14(1)
+
+// if(browser=='Edge'){
+//     alert( "You've got the Edge!" );
+// }else if(browser=='Opera'|| browser=='Safari'||browser=='Firefox'||browser=='Chrome'){
+//     alert( 'Okay we support these browsers too' );
+// }else if(browser==''||browser==null){
+//     alert( 'We hope that this page looks ok!' );   
+// }
+
+
+//2.14(2)
+
+const number = +prompt('Введите число между 0 и 3', '');
+
+switch(number){
+    case 0:
+        alert('Вы ввели число 0');
+    break;
+
+    case 1:
+        alert('Вы ввели число 1');
+    break;
+
+    case 2:
+    case 3:
+        alert('Вы ввели число 2, а может и 3');
+    break;
+}
